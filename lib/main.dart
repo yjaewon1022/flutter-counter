8b3f91e0ce1831954e4c796a53d3458159829cdf
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-18 11:34:39 +0900

    stateless widget 교재 내용 작성

--- lib/main.dart
+++ lib/main.dart
@@ -21,95 +21,99 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    final titleImage = _buildTitleImage(info['titleImageLink']);
+    Widget textSection = _buildTextSection(info['textSection']);
+    // 다크 테마 / 화이트 테마
+    Widget buttonSection = _buildButtonSection(Theme.of(context).primaryColor);
+    Widget titleSection = _buildTitleSection(
+      info['titleSectionHeader'],
+      info['titleSectionBody'],
+      info['titleSectionScore'],
     );
-  }
-}
 
-// StatefulWidget은 별다른 기능 없이 사용자가 값을 계속 변경할 수 있게 하는
-// State 를 생성하는 기능만 가지고 있다.
-class MyHomePage extends StatefulWidget {
-  // MyHomePage 클래스의 생성자
-  const MyHomePage({super.key, required this.title});
-
-  // 인스턴스 변수
-  final String title;
-
-  // 사용자가 앱 사용 중 계속해서 값을 변경할 수 있는 State를 생성할 수 있게 해주는 함수
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-// State<MyHomePage> : MyHomePage 라는 클래스의 State 입니다. 라는 것을 명시해주는 것
-class _MyHomePageState extends State<MyHomePage> {
-  // 인스턴스 변수 (사용자가 앱에서 실제로 변경할 수 있는 값)
-  int _counter = 0;
-
-  // _incrementCounter() : State 클래스 내에서 사용할 수 있는 기능(함수)
-  void _incrementCounter() {
-    // setState : 이 클래스에서 가지고 있는 인스턴스 변수의 값을 바꿀 때 사용하는 기능(함수)
-    setState(() {
-      _counter++;
-    });
-  }
-
-  // upperCamelCase 가 무엇인가?
-  // 항상 식별자의 첫번째 문자는 대문자로 표기 해야 하며, 만약 여러단어가 포함되어 있는 경우라면, 각 단어를 구분짓게 해주기 위해서 마찬가지로 대문자로 표기해야 한다.
-  // 사용방법 : (숫자 더하기 라는 기능을 하는 함수 이름을 짓고싶다.) add + number
-  // upperCamelCase 사용 시 : AddNumber
-  // lowerCamelCase : addNumber
-
-  // 빼기를 할 수 있는 기능
-  void decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  // State 로 선언된 클래스는 현재 내가 가지고 있는 값을 어떻게 반환할건지
-  // build() 기능을 통해서 외부(StatefulWidget인 MyHomePage)로 전달해주어야 함.
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      title: info['appTitle'],
+      home: Scaffold(
+        appBar: AppBar(title: Text(info['appBarTitle'])),
+        body: ListView(
+          children: [titleImage, titleSection, buttonSection, textSection],
         ),
       ),
-      // 이 부분을 수정해서 왼쪽 버튼을 누르면 -, 오른쪽 버튼을 누르면 + 하는 기능 추가
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: decrementCounter,
-            tooltip: 'Decrement',
-            child: const Text("-"),
-          ),
-          // 빈 칸을 뜻하는 클래스를 하나 추가할 수도 있음 : Padding
-          Padding(padding: EdgeInsets.all(10)),
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ],
-      ),
     );
   }
+}
+
+Image _buildTitleImage(String imageName) {
+  return Image.network(imageName, width: 600, height: 240, fit: BoxFit.cover);
+}
+
+Container _buildTitleSection(String name, String addr, int count) {
+  return Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Text(
+                  name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Text(addr, style: TextStyle(color: Colors.grey[500])),
+            ],
+          ),
+        ),
+        Icon(Icons.star, color: Colors.red[500]),
+        Text('$count'),
+      ],
+    ),
+  );
+}
+
+Widget _buildButtonSection(Color color) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(color, Icons.assistant_navigation, 'Visit'),
+      _buildButtonColumn(color, Icons.add_alert_sharp, 'Alarm'),
+      _buildButtonColumn(color, Icons.share, 'Share'),
+    ],
+  );
+}
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.w400,
+            color: color,
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Container _buildTextSection(String section) {
+  return Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      section,
+      softWrap: true,
+      textAlign: TextAlign.justify,
+      style: const TextStyle(height: 1.5, fontSize: 15),
+    ),
+  );
 }
