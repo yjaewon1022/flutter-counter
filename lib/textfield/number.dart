ef36814c9eca89d9589f298997d2c36fc4e35252
WhaleKings <rnjsxodbs9898@gmail.com>
2025-07-21 15:01:13 +0900

    숫자적용

--- lib/textfield/number.dart
+++ lib/textfield/number.dart
@@ -11,58 +11,63 @@ class NumberTextFieldWidget extends StatefulWidget {
 
 class NumberTextFieldState extends State<NumberTextFieldWidget> {
   late String _text = '';
+  late String _text2 = '';
+
   void changeText(String value) {
     setState(() {
       _text = value;
     });
   }
 
+  void changeText2(String value) {
+    setState(() {
+      _text2 = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Scaffold(
-          body: Container(
-            padding: EdgeInsets.all(10),
-            child: ListView(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    label: Text("숫자를 입력"),
-                    border: UnderlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    changeText(value);
-                  },
-                  keyboardType: TextInputType.number,
-                ),
-                Text(_text),
-              ],
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                label: Text("숫자를 입력"),
+                border: UnderlineInputBorder(),
+              ),
+              onChanged: (value) {
+                changeText(value);
+              },
+              keyboardType: TextInputType.number,
             ),
-          ),
-        ),
-        Scaffold(
-          body: Container(
-            padding: EdgeInsets.all(10),
-            child: ListView(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    label: Text("숫자를 입력"),
-                    border: UnderlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    changeText(value);
-                  },
-                  keyboardType: TextInputType.number,
-                ),
-                Text(_text),
-              ],
+
+            TextField(
+              decoration: InputDecoration(
+                label: Text("숫자를 입력"),
+                border: UnderlineInputBorder(),
+              ),
+              onChanged: (value) {
+                changeText2(value);
+              },
+              keyboardType: TextInputType.number,
             ),
-          ),
+            Text(
+              "$_text + $_text2의 결과는 ${int.parse(_text) + int.parse(_text2)}입니다.",
+            ),
+            Text(
+              "$_text + $_text2의 결과는 ${int.parse(_text) - int.parse(_text2)}입니다.",
+            ),
+            Text(
+              "$_text + $_text2의 결과는 ${int.parse(_text) * int.parse(_text2)}입니다.",
+            ),
+            Text(
+              "$_text + $_text2의 결과는 ${int.parse(_text) / int.parse(_text2)}입니다.",
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
