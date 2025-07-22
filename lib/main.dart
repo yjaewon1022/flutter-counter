af2b470c4fed87f8052f4337b06ab03e536966ba
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-22 10:43:46 +0900

    불필요한 코드 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
-// BottomNavigationBar 에 Color 라는 이름을 가진 Item을 하나 생성하고,
-// 해당 아이템을 클릭하면
-// 화면의 AppBar 부분에 ['좋아하는 색상 고르기'] 라는 이름을 가지도록 생성하고
-// 빨 주 노
-// 초 파 남
-// 보 흰 검
-// 에 해당하는 박스(Container)를 100 x 100 픽셀의 크기로 만들되,
-// 좌우 여백과 상하 여백이 존재하게 만들어보세요.
-
+import 'package:counter/day11/day11-quiz.dart';
 import 'package:counter/day9/note.dart';
-import 'package:counter/day11/textfield/button-input.dart';
 import 'package:flutter/material.dart';
-// import 'book.dart';
-// import 'gugudan.dart';
-// import 'star.dart';
-// import 'color-select.dart';
-// import 'hello.dart';
-import 'day11/textfield/normal.dart';
-// import 'textfield/number.dart';
-// import 'textfield/password.dart';
-// import 'textfield/multiline.dart';
-// import 'textfield/maxlength.dart';
-import 'day11/day11-quiz.dart';
 
 void main() => runApp(MyApp());
 
@@ -49,18 +29,7 @@ class MyStatefulWidgetState extends State<MyStatefulWidget> {
   );
 
   final List<Widget> _widgetOptions = <Widget>[
-    // ColorSelect(),
-    // Book(),
-    // Gugudan(),
-    // Star(),
-    // HelloWidget(),
-    NormalTextFieldWidget(),
-    // NumberTextFieldWidget(),
-    // PasswordTextFieldWidget(),
-    // MultiLineTextFieldWidget(),
-    // MaxLengthTextFieldWidget(),
     NoteWidget(),
-    // ButtonInputTextFieldWidget(),
     CalculatorWidget(),
   ];
 
@@ -90,28 +59,6 @@ class MyStatefulWidgetState extends State<MyStatefulWidget> {
             bottomNavigationBar: BottomNavigationBar(
               type: BottomNavigationBarType.fixed,
               items: const <BottomNavigationBarItem>[
-                // BottomNavigationBarItem(icon: Icon(Icons.color_lens), label: 'Color'),
-                // BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-                // BottomNavigationBarItem(icon: Icon(Icons.wb_cloudy), label: 'Hello'),
-                // BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Star'),
-                // BottomNavigationBarItem(icon: Icon(Icons.handshake), label: 'Dialog'),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.text_fields),
-                  label: "기본 입력창",
-                ),
-                // BottomNavigationBarItem(
-                //   icon: Icon(Icons.numbers_rounded),
-                //   label: '숫자 입력창',
-                // ),
-                // BottomNavigationBarItem(icon: Icon(Icons.lock), label: '비밀번호 입력창'),
-                // BottomNavigationBarItem(
-                //   icon: Icon(Icons.input_sharp),
-                //   label: '다중줄 입력창',
-                // ),
-                // BottomNavigationBarItem(
-                //   icon: Icon(Icons.text_decrease),
-                //   label: "글자수제한 입력창",
-                // ),
                 BottomNavigationBarItem(
                   icon: Icon(Icons.note_add),
                   label: "자소서 작성",
