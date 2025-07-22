c979091b881c678e844ecd784763f42d2c7b5824
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-22 17:53:45 +0900

    ElevatedButton 학습

---
+++ lib/day12/button/elevated-button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(ElevatedButtonWidget());
+}
+
+class ElevatedButtonWidget extends StatefulWidget {
+  @override
+  State<ElevatedButtonWidget> createState() => ElevatedButtonWidgetState();
+}
+
+class ElevatedButtonWidgetState extends State<ElevatedButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: ElevatedButton(
+          onPressed: () {
+            print('버튼 클릭됨!');
+          },
+          child: Text('클릭'),
+        ),
+      ),
+    );
+  }
+}
