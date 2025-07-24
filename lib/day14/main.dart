55dda379bcf79c4b8b42832fcacbdda497d0152f
yjaewon1022 <yjaewon1022@gmail.com>
2025-07-24 11:26:16 +0900

    audioplayers 패키지 설치 및 메인 화면 적용

--- lib/day14/main.dart
+++ lib/day14/main.dart
@@ -1,6 +1,7 @@
 // 외부 라이브러리 사용하기 실습
 // https://pub.dev/packages/url_launcher
 
+import 'package:counter/day14/audioplayer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -10,6 +11,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "외부 라이브러리 설치", home: MainPage());
+  }
+}
+
+class MainPage extends StatelessWidget {
   final Uri _url = Uri.parse('https://naver.com');
 
   Future<void> _launchUrl() async {
@@ -34,6 +42,21 @@ class MyApp extends StatelessWidget {
               // 버튼과 같이 사용자의 입력을 통해서 특정 사이트나 앱 으로 연결하고 싶을 때
               // url_launcher 라이브러리를 사용한다.
               ElevatedButton(onPressed: _launchUrl, child: Text("페이지 이동")),
+              ElevatedButton(
+                onPressed: () {
+                  // AudioPage 로 이동하게 해주는 기능 추가
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      // builder: (BuildContext context) => AudioPage(),
+                      builder: (BuildContext context) {
+                        return AudioPage();
+                      },
+                    ),
+                  );
+                },
+                child: Text("MP3 플레이어 화면으로 이동하기"),
+              ),
             ],
           ),
         ),
